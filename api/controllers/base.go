454f1f7079abbc141b3d5fb8cafe395e9c3b99a9
David Dinh <dmdinh22@gmail.com>
2020-01-10 23:12:49 -0800

    base controller

---
+++ api/controllers/base.go
@@ -0,0 +1,63 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql"    //mysql db driver
+	_ "github.com/jinzhu/gorm/dialects/postgres" //postgres db driver
+
+	"github.com/victorsteven/fullstack/api/models"
+)
+
+type Server struct {
+	DB     *gorm.DB
+	Router *mux.Router
+}
+
+//	  the receiver
+func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, DbName string) {
+	var err error
+
+	// api supports both mysql and postgresql - define which in env
+	if Dbdriver == "mysql" {
+		DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s):%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
+		server.DB err = gorm.Open(Dbdriver, DBURL)
+
+		if err != nil {
+			fmt.Printf("Cannot connect to %s db", Dbdriver)
+			log.Fatal("DB error:", err)
+		} else {
+			fmt.Printf("Successfully connected to the %s db", Dbdriver)
+		}
+	}
+
+		if Dbdriver == "postgres" {
+			DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
+
+			server.DB, err = gorm.Open(Dbdriver, DBURL)
+
+			if err != nil {
+				fmt.Printf("Cannot connect to %s db", Dbdriver)
+				log.Fatal("DB error:", err)
+			} else {
+				fmt.Printf("Successfully connected to the %s db", Dbdriver)
+			}
+		}
+
+		// run db migration
+		server.DB.Debug().AutoMigrate(&models.User{}, &models.Post{})
+
+		server.Router = mux.NewRouter()
+
+		server.initializeRoutes()
+}
+
+func (server *Server) Run(addr string) {
+	fmt.Println("Listening on port 8080. 🚀")
+	log.Fata(http.ListenAndServe(addr, server.Router))
+}
