a0c094bb639d4d5af628a7abbf81a51a3d39178e
David Dinh <dmdinh22@gmail.com>
2020-01-10 19:27:19 -0800

    post model

--- api/models/User.go
+++ api/models/User.go
@@ -13,12 +13,12 @@ import (
 )
 
 type User struct {
-	ID        uint32    `gorm:"primary_key;auto_increment" json:"id"`
-	Nickname  string    `gorm:"size:255;not null;unique" json:"nickname"`
-	Email     string    `gorm:"size:100;not null;unique" json:"email"`
-	Password  string    `gorm:"size:100;not null;" json:"password"`
-	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+	ID        uint32    `gorm:"primary_key;auto_increment" 	json:"id"`
+	Nickname  string    `gorm:"size:255;not null;unique" 		json:"nickname"`
+	Email     string    `gorm:"size:100;not null;unique" 		json:"email"`
+	Password  string    `gorm:"size:100;not null;" 					json:"password"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP"	 	json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" 	json:"updated_at"`
 }
 
 func Hash(password string) ([]byte, error) {
