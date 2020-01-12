3c33c603662766d4af5abbf8b149ba8ca5604a39
David Dinh <dmdinh22@gmail.com>
2020-01-12 01:52:31 -0800

    test update post

--- tests/controllertests/post_controller_test.go
+++ tests/controllertests/post_controller_test.go
@@ -211,3 +211,154 @@ func TestGetPostByID(t *testing.T) {
 		}
 	}
 }
+
+func TestUpdatePost(t *testing.T) {
+	var PostUserEmail, PostUserPassword string
+	var AuthPostAuthorID uint32
+	var AuthPostID uint64
+
+	err := refreshUserAndPostTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	users, posts, err := seedUsersAndPosts()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Get only the first user
+	for _, user := range users {
+		if user.ID == 2 {
+			continue
+		}
+		PostUserEmail = user.Email
+		PostUserPassword = "p@$$w0rd" //Note the password in the database is already hashed, we want unhashed
+	}
+	//Login the user and get the authentication token
+	token, err := server.SignIn(PostUserEmail, PostUserPassword)
+	if err != nil {
+		log.Fatalf("cannot login: %v\n", err)
+	}
+	tokenString := fmt.Sprintf("Bearer %v", token)
+
+	// Get only the first post
+	for _, post := range posts {
+		if post.ID == 2 {
+			continue
+		}
+		AuthPostID = post.ID
+		AuthPostAuthorID = post.AuthorID
+	}
+	// fmt.Printf("this is the auth post: %v\n", AuthPostID)
+
+	samples := []struct {
+		id           string
+		updateJSON   string
+		statusCode   int
+		title        string
+		content      string
+		authorId     uint32
+		tokenGiven   string
+		errorMessage string
+	}{
+		{
+			// Convert int64 to int first before converting to string
+			id:           strconv.Itoa(int(AuthPostID)),
+			updateJSON:   `{"title":"The updated post", "content": "This is the updated content", "authorId": 1}`,
+			statusCode:   200,
+			title:        "The updated post",
+			content:      "This is the updated content",
+			authorId:     AuthPostAuthorID,
+			tokenGiven:   tokenString,
+			errorMessage: "",
+		},
+		{
+			// When no token is provided
+			id:           strconv.Itoa(int(AuthPostID)),
+			updateJSON:   `{"title":"This is still another title", "content": "This is the updated content", "authorId": 1}`,
+			tokenGiven:   "",
+			statusCode:   401,
+			errorMessage: "Unauthorized",
+		},
+		{
+			// When incorrect token is provided
+			id:           strconv.Itoa(int(AuthPostID)),
+			updateJSON:   `{"title":"This is still another title", "content": "This is the updated content", "authorId": 1}`,
+			tokenGiven:   "this is an incorrect token",
+			statusCode:   401,
+			errorMessage: "Unauthorized",
+		},
+		{
+			//Note: "Title 2" belongs to post 2, and title must be unique
+			id:           strconv.Itoa(int(AuthPostID)),
+			updateJSON:   `{"title":"Title 2", "content": "This is the updated content", "authorId": 1}`,
+			statusCode:   500,
+			tokenGiven:   tokenString,
+			errorMessage: "Title has already been used.",
+		},
+		{
+			id:           strconv.Itoa(int(AuthPostID)),
+			updateJSON:   `{"title":"", "content": "This is the updated content", "authorId": 1}`,
+			statusCode:   422,
+			tokenGiven:   tokenString,
+			errorMessage: "Required Title",
+		},
+		{
+			id:           strconv.Itoa(int(AuthPostID)),
+			updateJSON:   `{"title":"Awesome title", "content": "", "authorId": 1}`,
+			statusCode:   422,
+			tokenGiven:   tokenString,
+			errorMessage: "Required Content",
+		},
+		{
+			id:           strconv.Itoa(int(AuthPostID)),
+			updateJSON:   `{"title":"This is another title", "content": "This is the updated content"}`,
+			statusCode:   401,
+			tokenGiven:   tokenString,
+			errorMessage: "Unauthorized",
+		},
+		{
+			id:         "unknwon",
+			statusCode: 400,
+		},
+		{
+			id:           strconv.Itoa(int(AuthPostID)),
+			updateJSON:   `{"title":"This is still another title", "content": "This is the updated content", "authorId": 2}`,
+			tokenGiven:   tokenString,
+			statusCode:   401,
+			errorMessage: "Unauthorized",
+		},
+	}
+
+	for _, v := range samples {
+		req, err := http.NewRequest("POST", "/posts", bytes.NewBufferString(v.updateJSON))
+
+		if err != nil {
+			t.Errorf("this is the error: %v\n", err)
+		}
+
+		req = mux.SetURLVars(req, map[string]string{"id": v.id})
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(server.UpdatePost)
+
+		req.Header.Set("Authorization", v.tokenGiven)
+
+		handler.ServeHTTP(rr, req)
+
+		responseMap := make(map[string]interface{})
+		err = json.Unmarshal([]byte(rr.Body.String()), &responseMap)
+		if err != nil {
+			t.Errorf("Cannot convert to json: %v", err)
+		}
+
+		assert.Equal(t, rr.Code, v.statusCode)
+		if v.statusCode == 200 {
+			assert.Equal(t, responseMap["Title"], v.title)
+			assert.Equal(t, responseMap["Content"], v.content)
+			assert.Equal(t, responseMap["AuthorID"], float64(v.authorId)) //just to match the type of the json we receive thats why we used float64
+		}
+
+		if v.statusCode == 401 || v.statusCode == 422 || v.statusCode == 500 && v.errorMessage != "" {
+			assert.Equal(t, responseMap["error"], v.errorMessage)
+		}
+	}
+}
