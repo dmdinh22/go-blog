fc3e4b08a925bd819b596393f2841fab124340c1
David Dinh <dmdinh22@gmail.com>
2020-04-04 19:30:16 -0700

    dev env

--- api/server.go
+++ api/server.go
@@ -24,20 +24,27 @@ func Run() {
 		fmt.Println("Loading env vars...")
 	}
 
-	if currentEnv != "production" {
+	if currentEnv == "dev" || currentEnv == "local" {
 		dbDriver = os.Getenv("DEV_DB_DRIVER")
 		dbUser = os.Getenv("DEV_DB_USER")
 		dbPassword = os.Getenv("DEV_DB_PASSWORD")
 		dbPort = os.Getenv("DEV_DB_PORT")
 		dbHost = os.Getenv("DEV_DB_HOST")
 		dbName = os.Getenv("DEV_DB_NAME")
-	} else {
+	} else if currentEnv == "production" {
 		dbDriver = os.Getenv("DB_DRIVER")
 		dbUser = os.Getenv("DB_USER")
 		dbPassword = os.Getenv("DB_PASSWORD")
 		dbPort = os.Getenv("DB_PORT")
 		dbHost = os.Getenv("DB_HOST")
 		dbName = os.Getenv("DB_NAME")
+	} else {
+		dbDriver = os.Getenv("TEST_DB_DRIVER")
+		dbUser = os.Getenv("TEST_DB_USER")
+		dbPassword = os.Getenv("TEST_DB_PASSWORD")
+		dbPort = os.Getenv("TEST_DB_PORT")
+		dbHost = os.Getenv("TEST_DB_HOST")
+		dbName = os.Getenv("TEST_DB_NAME")
 	}
 
 	server.Initialize(dbDriver, dbUser, dbPassword, dbPort, dbHost, dbName)
